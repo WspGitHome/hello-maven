124a66ec797bec17c3499edfe52c56e9a7f8a595
吴淑平 <sdlwwsp@163.com>
2020-12-23 17:05:07 +0800

    new content add

---
+++ maven-hello/src/main/java/com/maven/arithmetic/MergeSort.java
@@ -0,0 +1,16 @@
+package com.maven.arithmetic;
+
+/**
+ * @Packagename com.maven.arithmetic
+ * @Classname MergeSort
+ * @Description
+ * @Authors Mr.Wu
+ * @Date 2020/12/23 16:44
+ * @Version 1.0
+ */
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] array = new int[]{6, 4, 20, 2, 12, 9, 34, 999, 323, 3, 56, 76, 1, 5};
+
+    }
+}
